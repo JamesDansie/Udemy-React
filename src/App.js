e7a45a5321e26cba34c2231a0c1a6260272eb493
JamesDansie <jdansie8336@gmail.com>
2020-05-20 15:13:00 -0700

    refactored to use state hook, weird

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
-  render() {
+const app = props => {
+  
+  const [ personsState, setPersonsState ] = useState({
+    persons: [
+      { name: 'James', age: 32},
+      { name: 'Max', age: 28},
+      { name: 'Ron', age: 78}
+    ],
+  });
+
+  const [ otherState, setOtherState ] = useState('blah!');
+
+  console.log(personsState, otherState);
+
+  const switchNameHandler = () => {
+    // in hooks, state overrides. In class it just updates
+    setPersonsState({
+      persons: [
+        { name: 'Jimmah', age: 32},
+        { name: 'Max', age: 28},
+        { name: 'Ron', age: 68}
+      ]
+    });
+  }
+  
     return (
       <div className="App">
         <h1>
           Hi! This is a react app
         </h1>
         <p>this is actually working :D</p>
-        <Person name="James" age="32">My hobbies are: Motorcycles and Jiu Jitsu</Person>
-        <Person name="Max" age="29"/>
-        <Person name="Ron" age="78">Being a car making farmer bad ass</Person>
-
+        <button onClick={switchNameHandler}>Switch Name</button>
+        <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
+        <Person name={personsState.persons[1].name} age={personsState.persons[1].age} />
+        <Person name={personsState.persons[2].name} age={personsState.persons[2].age} />
       </div>
       );
 
       // what jsx gets compiled into.
       // return React.createElement('div', {className: 'App'}, React.createElement('h1',null, 'Hi this is an h1!'));
-  }
+  
 }
 
-export default App;
+export default app;
