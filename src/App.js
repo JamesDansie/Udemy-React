42445ccce7d21b63325de248199e3643ef82a99f
JamesDansie <jdansie8336@gmail.com>
2020-05-21 15:32:59 -0700

    added editting to each person

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import Person from './Person/Person';
 class App extends Component {
   state = {
     persons: [
-      { name: 'Max', age: 28 },
-      { name: 'Manu', age: 29 },
-      { name: 'Stephanie', age: 26 }
+      { id: 'asdf', name: 'Max', age: 28 },
+      { id: 'sdfg', name: 'Manu', age: 29 },
+      { id: 'qwer', name: 'Stephanie', age: 26 }
     ],
     otherState: 'some other value',
     showPersons: true
@@ -21,13 +21,23 @@ class App extends Component {
     this.setState({persons: persons});
   };
 
-  nameChangedHandler = (event) => {
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id == id;
+    });
+
+    // makes a copy of the person at that index
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    person.name = event.target.value;
+    
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
     this.setState({
-      persons: [
-        { name: 'Max', age: 28 },
-        { name: event.target.value, age: 29 },
-        { name: 'Stephanie', age: 27 }
-      ]
+      persons: persons
     })
   };
 
@@ -54,7 +64,9 @@ class App extends Component {
             return <Person 
             click={() => this.deletePersonHandler(index)}
             name={person.name} 
-            age={person.age}/>
+            age={person.age}
+            key={person.id}
+            changed={(event) => this.nameChangedHandler(event, person.id)} />
           })}
         </div>
       );
