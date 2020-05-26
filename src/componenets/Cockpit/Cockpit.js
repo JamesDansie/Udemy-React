2fb5182c8b063511f8dba57243b88a34c03114a8
JamesDansie <jdansie8336@gmail.com>
2020-05-25 21:58:01 -0700

    added a useEffect hook in cockpit

--- src/componenets/Cockpit/Cockpit.js
+++ src/componenets/Cockpit/Cockpit.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import classes from './Cockpit.css';
 
 const cockpit = (props) => {
+    // with blank dependancies (array), this will only run once.
+    useEffect(() => {
+        console.log('cockpit [useEffect]')
+
+        //fake http request
+        setTimeout(() => {
+            alert('loaded!');
+        }, 1000);
+
+        return () => {
+            // use effect clean up
+        }
+
+        // this array sets dependencies
+    }, [])
+
     let assignedClasses = [];
 
     let btnClass = '';
