dc9d283b226dc0e9a67095508048042876f9ee5f
JamesDansie <jdansie8336@gmail.com>
2020-05-25 21:24:58 -0700

    added some console logs to see lifecycle

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,6 +4,12 @@ import Persons from '../componenets/Persons/Persons';
 import Cockpit from '../componenets/Cockpit/Cockpit';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    console.log('App.js [constructor]');
+
+  }
+
   state = {
     persons: [
       { id: 'asdf', name: 'Max', age: 28 },
@@ -13,6 +19,15 @@ class App extends Component {
     otherState: 'some other value',
     showPersons: true
   };
+
+  static getDerivedStateFromProps(props, state){
+    console.log('App.js, [getDerivedStateFromProps]', props);
+    return state;
+  }
+
+  componentDidMount() {
+    console.log('App.js componenetDidMount');
+  }
 
   deletePersonHandler = (personIndex) => {
     // two ways of making a new array, rather than pointing to the old array.
@@ -48,6 +63,7 @@ class App extends Component {
   }
 
   render() {
+    console.log('App.js [render]')
 
     let persons = null;
 
